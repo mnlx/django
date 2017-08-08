ec8a6a7de8e21542e0155d608a6f5d70a3c34eba
mnlx <andreduarte2010@gmail.com>
2017-08-08 06:45:24 -0300

    Trying for pc

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,11 @@
 from rango import views
 from django.conf.urls import url
 
+
 app_name = 'rango'
+
+
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -10,11 +14,8 @@ urlpatterns = [
     url(regex=r'^category/(?P<category_name_slug>[\w\-]+)/$', view=views.category, name='category'),
     url(regex=r'^add_category/$', view=views.add_category, name='add_category'),
     url(regex=r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', view=views.add_page, name='add_page'),
-    url(r'^register/$', views.register, name='registration'),
-    url(r'^login/$', views.user_login, name='login'),
-    # url(r'^logout/$', views.signout, name='logout'),
-    url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^restricted/', views.restricted, name='restricted')
+
 
 
 ]
